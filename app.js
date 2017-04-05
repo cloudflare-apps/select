b6b4dd910ebb28d6c7c3953b91b991104adb6b45
Teffen Ellis (Eric) <teffen@nirri.us>
2017-04-05 02:32:53 -0700

    Limit package to used dependencies.

--- app.js
+++ app.js
@@ -1,31 +1,15 @@
 (function () {
   let options = INSTALL_OPTIONS
 
-  function updateElement() {
-    Select.init({
-      className: options.theme
+  function updateElement () {
+    window.Select.init({
+      className: `select-theme-${options.theme}`
     })
   }
 
-  if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", updateElement)
-  }
-  else {
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', updateElement)
+  } else {
     updateElement()
-  }
-
-  INSTALL_SCOPE = {
-    setOptions(nextOptions) {
-      if (!document.body.classList) return
-
-      const elements = Array.prototype.slice.call(document.querySelectorAll("." + options.theme))
-
-      elements.forEach(element => {
-        element.classList.remove(options.theme)
-        element.classList.add(nextOptions.theme)
-      })
-
-      options = nextOptions
-    }
   }
 }())
